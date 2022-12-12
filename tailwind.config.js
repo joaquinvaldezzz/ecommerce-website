38ba68b34ac275fc88aa2a1e274769ac0b7aca47
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-12 09:08:02 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.php', './*.html', './views/**/*.php'],
+  content: ['./*.php', './*.html', './includes/**/*.php'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+      },
+    },
+    extend: {
+      backgroundImage: {
+        'sign-in': "url('public/images/banner/banner-sign-in.jpg')",
+        'create-an-account': "url('public/images/banner/banner-create-an-account.jpg')",
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
