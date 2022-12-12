a0002bc5f61b6002416d9214dbb61deb80103bea
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-12 09:10:51 +0800

    Add .html files

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
-      content: ['./*.php', './*.html', './includes/**/*.php'],
+      content: ['./*.php', './*.html', './includes/**/*.{php,html}'],
       defaultExtractor: (content) => {
         const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
         const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.php', './*.html', './includes/**/*.php'],
+  content: ['./*.php', './*.html', './includes/**/*.{php,html}'],
   theme: {
     container: {
       center: true,
