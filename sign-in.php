d2579af8d18de05ee51df22c78bf90032ded4ded
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-13 13:03:31 +0800

    Update sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -31,12 +31,13 @@
               Sign in to your account
             </h1>
 
-            <form class="mt-6 space-y-6">
+            <form class="mt-6 space-y-6" method="post">
               <label class="block">
                 <p class="text-sm font-semibold">Email address</p>
                 <input
                   class="mt-2 block w-full rounded-lg border border-slate-300 px-3 py-2 focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2"
                   id="email-address"
+                  name="email-address"
                   type="email"
                 />
               </label>
@@ -46,6 +47,7 @@
                 <input
                   class="mt-2 block w-full rounded-lg border border-slate-300 px-3 py-2 focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:ring-offset-2"
                   id="password"
+                  name="password"
                   type="password"
                 />
               </label>
@@ -53,12 +55,37 @@
               <div>
                 <button
                   class="block w-full rounded-lg bg-indigo-600 px-3 py-2 text-white"
+                  name="log-in"
                   type="submit"
                 >
                   Log in
                 </button>
               </div>
             </form>
+
+            <p class="mt-4">
+              <?php
+              require 'database.php';
+
+              if (isset($_POST['log-in'])) {
+                $email_address = $_POST['email-address'];
+                $password = md5($_POST['password']);
+
+                $login_query = mysqli_query(
+                  $mysqli,
+                  "SELECT * FROM user_accounts WHERE email_address = '$email_address' AND password = '$password'"
+                );
+
+                $result = mysqli_fetch_array($login_query);
+
+                if ($result) {
+                  echo 'Login successfully.';
+                } else {
+                  echo 'Username and password did not match.';
+                }
+              }
+              ?>
+            </p>
           </div>
 
           <div
