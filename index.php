55406a5ee7bffa6e003752a0792976c8adb881b3
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-13 11:59:49 +0800

    Update title and define a global variable

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
+<?php define('MAIN_CSS', '/ecommerce-website/dist/main.css'); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>eCommerce Wesbite</title>
-    <link rel="stylesheet" href="dist/main.css" />
+    <title>eCommerce Website</title>
+    <link rel="stylesheet" href="<?php echo MAIN_CSS; ?>" />
     <link rel="icon" href="public/favicon.svg" />
   </head>
 
