56630c00ff9b3c2de891050e62e96cc988ed729d
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-15 10:48:49 +0800

    Update contents.js

--- contents.js
+++ contents.js
@@ -1,1 +1,7 @@
-module.exports = ['./*.html', './*.php', './html/**/*.html', './includes/**/*.{php,html}']
+module.exports = [
+  './*.html',
+  './*.php',
+  './html/**/*.html',
+  './includes/**/*.{php,html}',
+  './src/components/**/*.html',
+]
