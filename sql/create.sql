163f89d09011c29552275e5e7da80b6800ac6843
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-15 11:36:51 +0800

    Create shopping_cart table

--- sql/create.sql
+++ sql/create.sql
@@ -9,3 +9,15 @@ CREATE TABLE
     password VARCHAR(256) NOT NULL,
     date_created DATETIME NOT NULL
   );
+
+CREATE TABLE
+  shopping_cart (
+    item_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    item_name VARCHAR(256) NOT NULL,
+    item_color VARCHAR(256) NOT NULL,
+    item_price DECIMAL(10, 2) NOT NULL,
+    item_size VARCHAR(256) NOT NULL,
+    item_quantity INT NOT NULL,
+    item_account_id INT NOT NULL,
+    FOREIGN KEY (item_account_id) REFERENCES user_accounts (id)
+  );
