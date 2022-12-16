0fdc1ef6da771ca0bc911f7e9dc4b12035d86c91
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-16 22:02:15 +0800

    Create conditional rendering of elements

--- includes/header.php
+++ includes/header.php
@@ -19,11 +19,20 @@
     </button>
 
     <div class="ml-auto flex items-center">
-      <div class="hidden md:flex md:items-center">
-        <a class="text-sm font-medium" href="/ecommerce-website/sign-in.php">Sign in</a>
-        <span class="select-none text-sm text-slate-300 md:mx-6 lg:mx-8">|</span>
-        <a class="text-sm font-medium" href="/ecommerce-website/sign-up.php">Create an account</a>
-      </div>
+      <?php
+      session_start();
+
+      if (!isset($_SESSION['first_name'])) {
+        echo '<div class="hidden md:flex md:items-center">
+          <a class="text-sm font-medium" href="/ecommerce-website/sign-in.php">Sign in</a>
+          <span class="select-none text-sm text-slate-300 md:mx-6 lg:mx-8">|</span>
+          <a class="text-sm font-medium" href="/ecommerce-website/sign-up.php">Create an account</a>
+        </div>';
+      } else {
+        echo '<p class="text-sm">Hello, ' . $_SESSION['first_name'] . '!</p>';
+        echo '<a class="text-sm" href="/ecommerce-website/sign-out.php">Sign out</a>';
+      }
+      ?>
 
       <div class="flex items-center md:ml-6 lg:ml-8">
         <button>
