25c4f809f126c2c7b01f309e84349b7485e37000
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-17 13:11:49 +0800

    Delete obsolete if-else statement

--- sign-in.php
+++ sign-in.php
@@ -76,17 +76,6 @@
                 $login_query = mysqli_query($mysqli, $query);
                 $result = mysqli_fetch_array($login_query);
                 ($result = mysqli_query($mysqli, $query)) or die(mysql_error());
-
-                if (mysqli_num_rows($result) === 1) {
-                  while ($row = mysqli_fetch_array($result)) {
-                    $first_name = $row['first_name'];
-                  }
-
-                  $_SESSION['first_name'] = $first_name;
-                  header('Location: index.php');
-                } else {
-                  include 'src/alerts/incorrect-credentials.html';
-                }
                 ?>
 
               <?php if (mysqli_num_rows($result) === 1): ?>
