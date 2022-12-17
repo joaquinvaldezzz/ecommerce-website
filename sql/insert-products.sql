768cab82e436a0836b34e0fb2d0f755c22638a0d
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-17 22:04:15 +0800

    Create insert-products.sql

---
+++ sql/insert-products.sql
@@ -0,0 +1,37 @@
+INSERT INTO
+  products_table (
+    product_id,
+    product_image,
+    product_name,
+    product_color,
+    product_price
+  )
+VALUES
+  (
+    1001,
+    '/ecommerce-website/public/images/trending-products/tee-deep-forest.jpg',
+    "Men's Essential Tee",
+    'Deep Forest',
+    29.00
+  ),
+  (
+    1002,
+    '/ecommerce-website/public/images/trending-products/tee-canyon.jpg',
+    "Men's Essential Tee",
+    'Canyon',
+    29.00
+  ),
+  (
+    1003,
+    '/ecommerce-website/public/images/trending-products/tee-black.jpg',
+    "Men's Essential Tee",
+    'Black',
+    29.00
+  ),
+  (
+    1004,
+    '/ecommerce-website/public/images/trending-products/tee-sienna.jpg',
+    "Men's Essential Tee",
+    'Sienna',
+    29.00
+  );
