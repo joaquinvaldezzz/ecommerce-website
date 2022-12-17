ef44218393d6a25c8ca7a9f9e98e5b1c34b5c3ca
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-17 10:29:23 +0800

    Merge first and last_name to user_accounts

--- sql/create.sql
+++ sql/create.sql
@@ -5,23 +5,12 @@ USE ecommerce;
 CREATE TABLE
   user_accounts (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(256) NOT NULL,
+    last_name VARCHAR(256),
     email_address VARCHAR(256) NOT NULL,
     password VARCHAR(256) NOT NULL,
     date_created DATETIME NOT NULL
   );
-
-ALTER TABLE
-  user_accounts
-ADD COLUMN
-  first_name VARCHAR(256) NOT NULL AFTER id;
-
-ALTER TABLE
-  user_accounts
-ADD COLUMN
-  last_name VARCHAR(256) NOT NULL after first_name;
-
-ALTER TABLE
-  user_accounts MODIFY COLUMN last_name VARCHAR(256);
 
 CREATE TABLE
   shopping_cart (
@@ -35,5 +24,6 @@ CREATE TABLE
     FOREIGN KEY (item_account_id) REFERENCES user_accounts (id)
   );
 
+-- Rest AUTO_INCREMENT of `id` in `user_accounts`
 ALTER TABLE
   user_accounts auto_increment = 1;
