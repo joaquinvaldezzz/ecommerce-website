240478d491b934fc0e0910bb4406a7d68bb98c67
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-21 15:31:17 +0800

    Loop data from the database

--- shopping-cart.php
+++ shopping-cart.php
@@ -17,158 +17,78 @@
         <div class="mt-8 md:mx-auto md:max-w-lg lg:mx-0 lg:max-w-none" role="grid">
           <div class="grid gap-8 lg:grid-cols-2 xl:grid-cols-3" role="row">
             <div class="xl:col-span-2" role="gridcell">
-              <div class="space-y-8 divide-y divide-slate-300">
-                <div class="flex">
-                  <div class="w-32 lg:w-40 xl:w-64">
-                    <img
-                      class="rounded-lg"
-                      src="/ecommerce-website/public/images/trending-products/tee-deep-forest.jpg"
-                      alt="Deep Forest Tee"
-                    />
-                  </div>
+              <?php
+              include 'includes/db_controller.php';
 
-                  <div class="ml-4 flex flex-1 flex-col">
-                    <div class="flex justify-between">
-                      <div>
-                        <h2 class="font-semibold">Men's Essential Tee</h2>
-                        <div class="mt-1 flex items-center space-x-2">
-                          <span class="text-sm text-slate-500">Deep Forest</span>
-                          <span class="select-none text-sm text-slate-300">|</span>
-                          <span class="text-sm text-slate-500">Large</span>
+              $db_handle = new DBController();
+              $cart_items = $db_handle->run_query('SELECT * FROM shopping_cart');
+              ?>
+
+              <div class="space-y-8 divide-y divide-slate-300 children:pt-8">
+                <?php if (empty($cart_items)): ?>
+                  <p>Your cart is empty.</p>
+                <?php else: ?>
+                  <?php foreach ($cart_items as $key => $value): ?>
+                  <div class="flex">
+                    <div class="w-32 lg:w-40 xl:w-64">
+                      <img
+                        class="rounded-lg"
+                        src="<?php echo $cart_items[$key]['item_image']; ?>"
+                        alt="<?php echo $cart_items[$key]['item_color']; ?> Tee"
+                      />
+                    </div>
+
+                    <div class="ml-4 flex flex-1 flex-col">
+                      <div class="flex justify-between">
+                        <div>
+                          <h2 class="font-semibold">
+                            <?php echo $cart_items[$key]['item_name']; ?>
+                          </h2>
+                          <div class="mt-1 flex items-center space-x-2">
+                            <span class="text-sm text-slate-500">
+                              <?php echo $cart_items[$key]['item_color']; ?>
+                            </span>
+                            <span class="select-none text-sm text-slate-300">|</span>
+                            <span class="text-sm text-slate-500">
+                              <span>Quantity: </span>
+                              <?php echo $cart_items[$key]['item_quantity']; ?>
+                            </span>
+                          </div>
+                          <div class="mt-1 text-sm font-bold">
+                            $<?php echo $cart_items[$key]['item_price']; ?>
+                          </div>
                         </div>
-                        <div class="mt-1 text-sm font-bold">$29.00</div>
+
+
+                        <button class="h-min">
+                          <svg class="h-6 w-6 text-slate-400" fill="none" viewBox="0 0 24 24">
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="m6 6 12 12M6 18 18 6 6 18Z"
+                            />
+                          </svg>
+                        </button>
                       </div>
 
-                      <div class="hidden sm:relative sm:flex sm:h-min">
-                        <select
-                          class="relative z-10 cursor-pointer appearance-none rounded-lg border bg-transparent py-2 pl-4 pr-8 text-sm focus:outline-none"
-                          id="quantity"
-                        >
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                        </select>
-
-                        <svg
-                          class="h-4 w-4 text-slate-500 sm:absolute sm:top-1/2 sm:right-2 sm:-translate-y-1/2"
-                          fill="none"
-                          viewBox="0 0 17 16"
-                        >
+                      <div class="mt-auto flex items-center">
+                        <svg class="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20">
                           <path
                             stroke="currentColor"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
-                            d="M13.167 6 8.5 10.667 3.833 6"
+                            d="M4.167 10.833 7.5 14.167l8.333-8.334"
                           />
                         </svg>
+                        <span class="ml-2 text-sm">In stock</span>
                       </div>
-
-                      <button class="h-min">
-                        <svg class="h-6 w-6 text-slate-400" fill="none" viewBox="0 0 24 24">
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m6 6 12 12M6 18 18 6 6 18Z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-
-                    <div class="mt-auto flex items-center">
-                      <svg class="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20">
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M4.167 10.833 7.5 14.167l8.333-8.334"
-                        />
-                      </svg>
-                      <span class="ml-2 text-sm">In stock</span>
                     </div>
                   </div>
-                </div>
-
-                <div class="flex pt-8">
-                  <div class="w-32 lg:w-40 xl:w-64">
-                    <img
-                      class="rounded-lg"
-                      src="/ecommerce-website/public/images/trending-products/tee-black.jpg"
-                      alt="Black Tee"
-                    />
-                  </div>
-
-                  <div class="ml-4 flex flex-1 flex-col">
-                    <div class="flex justify-between">
-                      <div>
-                        <h2 class="font-semibold">Men's Essential Tee</h2>
-                        <div class="mt-1 flex items-center space-x-2">
-                          <span class="text-sm text-slate-500">Black</span>
-                          <span class="select-none text-sm text-slate-300">|</span>
-                          <span class="text-sm text-slate-500">Large</span>
-                        </div>
-                        <div class="mt-1 text-sm font-bold">$29.00</div>
-                      </div>
-
-                      <div class="hidden sm:relative sm:flex sm:h-min">
-                        <select
-                          class="relative z-10 cursor-pointer appearance-none rounded-lg border bg-transparent py-2 pl-4 pr-8 text-sm focus:outline-none"
-                          id="quantity"
-                        >
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                        </select>
-
-                        <svg
-                          class="h-4 w-4 text-slate-500 sm:absolute sm:top-1/2 sm:right-2 sm:-translate-y-1/2"
-                          fill="none"
-                          viewBox="0 0 17 16"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M13.167 6 8.5 10.667 3.833 6"
-                          />
-                        </svg>
-                      </div>
-
-                      <button class="h-min">
-                        <svg class="h-6 w-6 text-slate-400" fill="none" viewBox="0 0 24 24">
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m6 6 12 12M6 18 18 6 6 18Z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-
-                    <div class="mt-auto flex items-center">
-                      <svg class="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20">
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M4.167 10.833 7.5 14.167l8.333-8.334"
-                        />
-                      </svg>
-                      <span class="ml-2 text-sm">In stock</span>
-                    </div>
-                  </div>
-                </div>
+                  <?php endforeach; ?>
+                <?php endif; ?>
               </div>
             </div>
 
