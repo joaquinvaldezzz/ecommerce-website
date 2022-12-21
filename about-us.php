6a71964659e96a6eb9c39390b2a50a783bd7e4cc
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-21 22:02:27 +0800

    Create about-us.php

---
+++ about-us.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About us</title>
+    <link rel="stylesheet" href="/ecommerce-website/dist/main.css" />
+    <link rel="icon" href="/ecommerce-website/public/favicon.svg" />
+  </head>
+
+  <body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+     <section class="py-8 sm:pb-12 md:pb-16">
+        <div class="container">
+          <div class="max-w-prose">
+            <h1 class="text-3xl font-extrabold tracking-tight lg:text-4xl">About us</h1>
+            <p class="mt-4 text-slate-500">
+              As the pandemic emerges globally, the team acknowledges the difficulty of going to the
+              mall and buying things that consumers want. The team completed this project because
+              they believed that as the internet grows in popularity, it should be used wisely by
+              creating an electronic shop on a website that allows consumers to stay safe during
+              pandemics and save time shopping. Online shopping is a type of electronic commerce
+              that allows users to order items they require simply by using their mobile or web
+              browser.
+            </p>
+
+            <h1 class="mt-8 text-2xl font-extrabold tracking-tight lg:text-3xl">Mission</h1>
+            <p class="mt-4 text-slate-500">
+              Our website is a fast-growing e-commerce shop that helps consumers save their precious
+              time and stay safe. The electronic shop was made with only one goal: to give happiness
+              to the consumer or buyer and help them give love and care to others by giving gifts.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
