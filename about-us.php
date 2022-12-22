db85483b8e07e7f0458a3b919050299972580c86
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-22 20:49:12 +0800

    Add a header background image

--- about-us.php
+++ about-us.php
@@ -11,27 +11,26 @@
   <body>
     <?php include 'includes/header.php'; ?>
     <main>
-     <section class="py-8 sm:pb-12 md:pb-16">
-        <div class="container">
-          <div class="max-w-prose">
-            <h1 class="text-3xl font-extrabold tracking-tight lg:text-4xl">About us</h1>
-            <p class="mt-4 text-slate-500">
-              As the pandemic emerges globally, the team acknowledges the difficulty of going to the
-              mall and buying things that consumers want. The team completed this project because
-              they believed that as the internet grows in popularity, it should be used wisely by
-              creating an electronic shop on a website that allows consumers to stay safe during
-              pandemics and save time shopping. Online shopping is a type of electronic commerce
-              that allows users to order items they require simply by using their mobile or web
-              browser.
-            </p>
+      <section class="min-h-25 bg-about-us bg-cover bg-center bg-no-repeat"></section>
 
-            <h1 class="mt-8 text-2xl font-extrabold tracking-tight lg:text-3xl">Mission</h1>
-            <p class="mt-4 text-slate-500">
-              Our website is a fast-growing e-commerce shop that helps consumers save their precious
-              time and stay safe. The electronic shop was made with only one goal: to give happiness
-              to the consumer or buyer and help them give love and care to others by giving gifts.
-            </p>
-          </div>
+      <section class="p-8 sm:pb-12 md:pb-16">
+        <div class="mx-auto max-w-prose">
+          <h1 class="text-3xl font-extrabold tracking-tight lg:text-4xl">About us</h1>
+          <p class="mt-4 leading-prose text-slate-500">
+            As the pandemic emerges globally, the team acknowledges the difficulty of going to the
+            mall and buying things that consumers want. The team completed this project because they
+            believed that as the internet grows in popularity, it should be used wisely by creating
+            an electronic shop on a website that allows consumers to stay safe during pandemics and
+            save time shopping. Online shopping is a type of electronic commerce that allows users
+            to order items they require simply by using their mobile or web browser.
+          </p>
+
+          <h1 class="mt-8 text-2xl font-extrabold tracking-tight lg:text-3xl">Mission</h1>
+          <p class="mt-4 leading-prose text-slate-500">
+            Our website is a fast-growing e-commerce shop that helps consumers save their precious
+            time and stay safe. The electronic shop was made with only one goal: to give happiness
+            to the consumer or buyer and help them give love and care to others by giving gifts.
+          </p>
         </div>
       </section>
     </main>
